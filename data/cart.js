aeccaa0f8a7827c206378846ceb4c329c84f8f8a
QuachKhang <phuckhang1088@gmail.com>
2025-03-09 14:19:45 +0700

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,20 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+if (cart.length === 0) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+const saveToStorage = () => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
 
 export const addToCart = (productId) => {
   let matchingcartItem;
@@ -32,6 +39,8 @@ export const addToCart = (productId) => {
       quantity,
     });
   }
+
+  saveToStorage();
 };
 
 export const removeFromCart = (productId) => {
@@ -43,4 +52,5 @@ export const removeFromCart = (productId) => {
   });
 
   cart = newCart;
+  saveToStorage();
 };
